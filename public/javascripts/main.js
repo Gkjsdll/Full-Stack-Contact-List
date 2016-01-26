919ec21d3747f539dc6a85da47a01e98c13a5d16
Zack Winchell <zackwinchell@gmail.com>
2016-01-25 22:53:27 -0800

    moved contact data into a single data key

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -12,9 +12,7 @@ function popContacts() {
       for(key in contacts){
         var contact = $("<tr>").addClass("contact");
         contact.append($('<td>').text(contacts[key].fullName));
-        contact.data("phone", contacts[key].phone);
-        contact.data("address", contacts[key].address);
-        contact.data("email", contacts[key].email);
+        contact.data("contactData", contacts[key])
         $contacts.push(contact);
       }
       $("#contactList").append($contacts);
